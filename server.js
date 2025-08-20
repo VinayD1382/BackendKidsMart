6dcd33ef8ea56ec0de730722604a8114fa12a03b
Vinay D <114236808+VinayD1382@users.noreply.github.com>
2025-08-20 10:34:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,10 @@ import orderPays from "./Router/OrdersPay.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL, 
+  credentials: true
+}));
 app.use(express.json());
 
 const MONGO_URI =
