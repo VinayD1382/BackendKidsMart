a08e96b35f72f58dae8ec58a2200fff50ddb72a4
Vinay D <114236808+VinayD1382@users.noreply.github.com>
2025-08-20 14:10:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,10 +31,26 @@ dotenv.config();
 const app = express();
 
 // CORS setup for Netlify frontend
-app.use(cors({
-  origin: "https://kidsmartshop.netlify.app",
-  credentials: true
-}));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://kidsmartshop.netlify.app"
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(express.json());
 
 // MongoDB connection
